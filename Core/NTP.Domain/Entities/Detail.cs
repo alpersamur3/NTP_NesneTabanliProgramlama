97b338d85dedba605e3a87572600cc148127a91e
alper <alpersamur0705@gmail.com>
2026-03-30 22:43:30 +0300

    API tarafında Production ve Development olarak çalışma alanları

---
+++ Core/NTP.Domain/Entities/Detail.cs
@@ -0,0 +1,26 @@
+﻿using NTP.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTP.Domain.Entities
+{
+    public class Detail:EntityBase
+    {
+        public Detail() { }
+
+        public Detail(string title, string value, int categoryId)
+        {
+            Title = title;
+            Value = value;
+            CategoryId = categoryId;
+        }
+
+        public string Title { get; set; }
+        public string Value { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
